4c3ac56cb34fce99f3661bd9c5a2545ddd78898c
Tobias_Yuchen <52252536+117999@users.noreply.github.com>
2021-12-06 13:29:16 +0800

    first trial for uploading self-developed module

---
+++ pysegment.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  6 13:25:32 2021
+
+@author: Tobias Vendetta
+"""
+
+'''
+pysegment: a small utility for cellular segementation from HE stained images
+'''
+### import libraries
+import matplotlib.pyplot as plt
+from skimage.io import imread, imsave
+
